dd69c49736baca4ca0285d3be275cb59a54923ea
Jan's MacBook <jan.zeller@hotmail.ch>
2025-10-26 10:50:44 +0100

    v2 start

--- aufgabe_1/sketch.js
+++ aufgabe_1/sketch.js
@@ -1,33 +1,44 @@
 // init variables
-let valueSlider;
 let sizeWidth = 1;
 let sizeHeight = 1;
+
+let minSize = 50;
+let maxSize; // depends on screen width
+let circles = [];
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // creates slider 0 - 100
-  valueSlider = createSlider(0, 100, 0);
-  valueDifferenceSlider = createSlider(0, 100, 50);
-
-  // position of slider in bottom center of screen
-  valueSlider.position(windowWidth / 2 - 100, windowHeight - 100);
-  valueDifferenceSlider.position(windowWidth / 2 - 100, windowHeight - 140);
+  growthSlider = createSlider(0, 10, 2, 0.1);
+  chaosSlider = createSlider(0, 5, 0, 0.1);
 
   // no outlines for circles
   noStroke();
+
+
+  for (let i = 0; i < 4; i++) {
+    circles.push({
+      size: minSize + i * 20,
+      growing: true
+    });
+  }
 }
 
 function draw() {
+  // position of slider in bottom center of screen
+  growthSlider.position(windowWidth / 2 - 100, windowHeight - 100);
+  chaosSlider.position(windowWidth / 2 - 100, windowHeight - 140);
 
   // map set slider values
-  let growSpeed = map(valueSlider.value(), 0, 100, 1, 50);
-  let differenceValue = map(valueDifferenceSlider.value(), 0, 100, 0, 300);
+  let growValue = map(growthSlider.value(), 0, 100, 1, 50);
+  let chaosValue = map(chaosSlider.value(), 0, 100, 0, 300);
 
-  let secondCircle = map(differenceValue, 100, 250, 0, 100);
-  let thirdCircle = map(differenceValue, 200, 500, 0, 100);
-  let fourthCircle = map(differenceValue, 100, 350, 0, 100);
+  let secondCircle = map(chaosValue, 100, 250, 0, 100);
+  let thirdCircle = map(chaosValue, 200, 500, 0, 100);
+  let fourthCircle = map(chaosValue, 100, 350, 0, 100);
 
   // Mouse for color 
   let mouseMap = map(mouseX, 0, 1600, 0, 255);
@@ -54,13 +65,6 @@ function draw() {
   fill(mouseMap, 100, 255);
   ellipse(windowWidth / 2, windowHeight / 2, sizeWidth - fourthCircle, sizeHeight - fourthCircle)
 
-  // // add text in white
-  // if (sizeWidth >= 1000) {
-  //   fill(255, 255, 255);
-  //   textSize(300);
-  //   textAlign(CENTER);
-  //   text('GROW', windowWidth / 2, windowHeight / 2 + 100);
-  // }
 
   // RESET
   if (sizeWidth >= windowWidth + 500) {
@@ -68,6 +72,10 @@ function draw() {
     sizeHeight = 1;
   }
 
-  sizeWidth = sizeWidth + growSpeed;
-  sizeHeight = sizeHeight + growSpeed;
+  sizeWidth = sizeWidth + growValue;
+  sizeHeight = sizeHeight + growValue;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
