b722aee84b7b38a1a756af8e9e167233e96a88d0
Jan's MacBook <jan.zeller@hotmail.ch>
2025-10-26 12:05:44 +0100

    v2

--- aufgabe_1/sketch.js
+++ aufgabe_1/sketch.js
@@ -1,80 +1,87 @@
 // init variables
-let sizeWidth = 1;
-let sizeHeight = 1;
-
 let minSize = 50;
 let maxSize; // depends on screen width
 let circles = [];
-
-
+let growing = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // creates slider 0 - 100
-  growthSlider = createSlider(0, 10, 2, 0.1);
-  chaosSlider = createSlider(0, 5, 0, 0.1);
+  growthSlider = createSlider(0, 50, 1, 1);
+  chaosSlider = createSlider(0, 50, 0, 1);
 
   // no outlines for circles
   noStroke();
 
+  background(255);
 
   for (let i = 0; i < 4; i++) {
-    circles.push({
-      size: minSize + i * 20,
-      growing: true
-    });
+    // set initial difference
+    let difference = i * 10;
+    circles[i] = minSize - difference;
+
   }
 }
 
 function draw() {
+
+  background(255);
   // position of slider in bottom center of screen
   growthSlider.position(windowWidth / 2 - 100, windowHeight - 100);
   chaosSlider.position(windowWidth / 2 - 100, windowHeight - 140);
 
+  // set max size of circles
+  maxSize = windowWidth + 200;
+
   // map set slider values
-  let growValue = map(growthSlider.value(), 0, 100, 1, 50);
-  let chaosValue = map(chaosSlider.value(), 0, 100, 0, 300);
+  let speed = map(growthSlider.value(), 0, 50, 1, 10);
+  let chaos = map(chaosSlider.value(), 0, 50, 0, 10);
 
-  let secondCircle = map(chaosValue, 100, 250, 0, 100);
-  let thirdCircle = map(chaosValue, 200, 500, 0, 100);
-  let fourthCircle = map(chaosValue, 100, 350, 0, 100);
+  console.log(chaos);
 
-  // Mouse for color 
-  let mouseMap = map(mouseX, 0, 1600, 0, 255);
+  // // Mouse for color 
+  // let mouseMap = map(mouseX, 0, 1600, 0, 255);
 
 
-  background(255);
-
-  // text grow in pink
+  // create text grow
   fill(0, 0, 0);
   textSize(300);
   textAlign(CENTER);
   text('GROW', windowWidth / 2 - 85, windowHeight / 2 + 105);
 
-  // ellipse 
-  fill(mouseMap, 0, 0);
-  ellipse(windowWidth / 2, windowHeight / 2, sizeWidth, sizeHeight)
+  for (let i = 0; i < 4; i++) {
 
-  fill(mouseMap, 255, 0);
-  ellipse(windowWidth / 2, windowHeight / 2, sizeWidth - secondCircle, sizeHeight - secondCircle)
+    // different circles get different growth when chaos kicks in
+    let growthRate = speed + (i * chaos);
 
-  fill(mouseMap, 255, 255);
-  ellipse(windowWidth / 2, windowHeight / 2, sizeWidth - thirdCircle, sizeHeight - thirdCircle)
+    // let smallest circle grow the biggest
+    let nr = 3 - i;
 
-  fill(mouseMap, 100, 255);
-  ellipse(windowWidth / 2, windowHeight / 2, sizeWidth - fourthCircle, sizeHeight - fourthCircle)
+    if (growing) {
+      circles[nr] += growthRate;
+    }
+    else {
+      circles[nr] -= growthRate;
+    }
+
+    // create circle
+    fill(50 + i * 50, 255 * i / 4, 255 - i * 50);
+    ellipse(windowWidth / 2, windowHeight / 2, circles[i], circles[i]);
 
 
-  // RESET
-  if (sizeWidth >= windowWidth + 500) {
-    sizeWidth = 1;
-    sizeHeight = 1;
+    // check if maxSize reached 
+    if (growing && circles[i] >= maxSize) {
+      growing = false;
+    }
+    else if (!growing && circles[i] <= minSize) {
+      growing = true;
+    }
+
+
   }
-
-  sizeWidth = sizeWidth + growValue;
-  sizeHeight = sizeHeight + growValue;
 }
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
