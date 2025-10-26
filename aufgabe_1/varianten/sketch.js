8773064dd555be5b6cf4171340b016a285eafee7
Jan's MacBook <jan.zeller@hotmail.ch>
2025-10-26 16:30:01 +0100

    added first version to varianten

--- aufgabe_1/varianten/sketch.js
+++ aufgabe_1/varianten/sketch.js
@@ -1,5 +1,5 @@
-// init variables
 let valueSlider;
+
 let sizeWidth = 1;
 let sizeHeight = 1;
 
@@ -7,24 +7,22 @@ let sizeHeight = 1;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // creates slider 0 - 100
+  // creates Slider 0 - 100
   valueSlider = createSlider(0, 100, 0);
-  valueDifferenceSlider = createSlider(0, 100, 50);
-
-  // position of slider in bottom center of screen
+  valueDifferenceSlider = createSlider(100, 1000, 500);
+  // position of Slider in bottom center of screen
   valueSlider.position(windowWidth / 2 - 100, windowHeight - 100);
   valueDifferenceSlider.position(windowWidth / 2 - 100, windowHeight - 140);
+  
 
-  // no outlines for circles
   noStroke();
 }
 
 function draw() {
+  let outputGrow = valueSlider.value();
+  let growSpeed = map(outputGrow, 0, 200, 1, 50);
 
-  // map set slider values
-  let growSpeed = map(valueSlider.value(), 0, 100, 1, 50);
-  let differenceValue = map(valueDifferenceSlider.value(), 0, 100, 0, 300);
-
+  let differenceValue = valueDifferenceSlider.value();
   let secondCircle = map(differenceValue, 100, 250, 0, 100);
   let thirdCircle = map(differenceValue, 200, 500, 0, 100);
   let fourthCircle = map(differenceValue, 100, 350, 0, 100);
