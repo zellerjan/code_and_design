4ab267ea54c650f1c7238c1abbfc7b5386a3ac84
Jan's MacBook <jan.zeller@hotmail.ch>
2025-11-09 13:50:50 +0100

    update stop start game

--- aufgabe_2/sketch.js
+++ aufgabe_2/sketch.js
@@ -1,3 +1,4 @@
+// init variables
 let numRects = 10;
 let rectW = 50;
 let rectH = 50;
@@ -7,13 +8,15 @@ let rectY = [];
 let rectSpeed = [];
 
 let gameStarted = false;
+let gamePaused = false;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   noStroke();
 
-  // create rectangles
+  // init rectangles
   for (let i = 0; i < numRects; i++) {
     rectX[i] = 10;
     rectY[i] = 100 + i * 80;
@@ -21,14 +24,25 @@ function setup() {
   }
 }
 
+
+
+
 function draw() {
   background(255);
 
   for (let i = 0; i < numRects; i++) {
-    fill(200, 0, 0);
+
+    // set color of rect based on red/green light
+    if (gamePaused) {
+      fill(200, 0, 0);
+    }
+    else {
+      fill(0, 200, 0);
+    }
     rect(rectX[i], rectY[i], rectW, rectH);
   }
 
+  // INTRO INFOS
   if (!gameStarted) {
     fill(0);
     textFont('Courier New');
@@ -38,7 +52,7 @@ function draw() {
     textStyle(BOLD);
     textAlign(CENTER, CENTER);
     text("Red Light – Green Light", width / 2, height / 2 - 50);
-  
+
     // display instruction to start
     push()
     fill(150);
@@ -55,12 +69,15 @@ function draw() {
   }
   else {
 
-    for (let i = 0; i < numRects; i++) {
-      rectX[i] += rectSpeed[i];
+    // regular movement of rectangles
+    if (!gamePaused) {
+      for (let i = 0; i < numRects; i++) {
+        rectX[i] += rectSpeed[i];
 
-      // bounce back on right side
-      if (rectX[i] > width - rectW || rectX[i] < 0) {
-        rectSpeed[i] *= -1;
+        // bounce back on right side
+        if (rectX[i] > width - rectW || rectX[i] < 0) {
+          rectSpeed[i] *= -1;
+        }
       }
     }
 
@@ -70,16 +87,37 @@ function draw() {
     textStyle(ITALIC);
     text('Press "SpaceBar" to stop – Eliminate with "click"', width / 2, height - 50);
   }
+
+  // console.log(numRects);
 }
 
+// Start game on click
 function mouseClicked() {
   if (!gameStarted) {
-    gameStarted = true; // Spiel starten
+    gameStarted = true;
+  }
+  else {
+    // check if rect got eliminated and reduce numRects
+    for (let i = numRects - 1; i >= 0; i--) {
+      if (mouseX > rectX[i] && mouseX < rectX[i] + rectW &&
+        mouseY > rectY[i] && mouseY < rectY[i] + rectH) {
+        rectX.splice(i, 1);
+        rectY.splice(i, 1);
+        rectSpeed.splice(i, 1);
+        numRects--;
+      }
+    }
   }
 }
 
 function keyPressed() {
+  // save result as screenshot
   if (key == 's' || key == 'S') {
     saveCanvas('Jans-Aufgabe-2', 'png');
+  }
+
+  // space bar to pause game
+  if (gameStarted && key == ' ') {
+    gamePaused = !gamePaused;
   }
 }
